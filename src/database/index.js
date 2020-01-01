1b1a5f39a928c4bb24ad3b9792cc7972c6d2d656
Gustavo <girardigustavo.03@gmail.com>
2020-01-01 14:26:05 -0300

    update database/index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/noderest',{ useUnifiedTopology: true
-});
+mongoose.connect('mongodb://localhost/noderest',{ useUnifiedTopology: true});
 
 mongoose.Promise = global.Promise;
 
